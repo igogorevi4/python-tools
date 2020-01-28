8ab7e9c4fd58b45e52805c2c5494863337ef80df
Yaroslav Pozdnyakov <yaroslav.pozdnyakov@gmail.com>
2020-01-28 12:19:04 +0000

    added auth options

--- rotate.artifacts.in.artifactory.py
+++ rotate.artifacts.in.artifactory.py
@@ -4,35 +4,38 @@ from artifactory import ArtifactoryPath
 import datetime
 
 retention_period = 7
-# put you artifactory path here
 # url = "http://repo.jfrog.org/artifactory/gradle-ivy-local"
+# put you artifactory path here
 url = "https://artifactory.example.com/repo-name"
+# put you access token here
+api_key = "ExampleApiToken"
 today = datetime.datetime.now()
 
 
-def get_age_of_artifact(artifact_url):
-    properties = ArtifactoryPath.stat(artifact_url)
-    creation_date = properties.ctime
+def get_latest_version_age(artifact_url):
+    path = ArtifactoryPath(artifact_url, apikey=api_key)
+    properties = ArtifactoryPath.stat(path)
+    modification_date = properties.mtime
     # removing timezone
-    creation_date = creation_date.replace(tzinfo=None)
-    aritfact_age = today - creation_date
+    modification_date = modification_date.replace(tzinfo=None)
+    aritfact_age = today - modification_date
     return aritfact_age.days
 
 
 def delete_artifact(artifact_url):
-    url = ArtifactoryPath(artifact_url)
+    url = ArtifactoryPath(artifact_url, apikey=api_key)
     if url.exists():
         url.unlink()
 
 
 def get_artifacts_urls(artifactory):
-    path = ArtifactoryPath(artifactory)
+    path = ArtifactoryPath(artifactory, apikey=api_key)
     return path
 
 
 def rotate_artifacts(artifactory_url):
     for artifact_url in get_artifacts_urls(artifactory_url):
-        artifactory_age = int(get_age_of_artifact(artifact_url))
+        artifactory_age = int(get_latest_version_age(artifact_url))
         if artifactory_age > retention_period:
             print("Deleting the artirfact: {0}; artifact age: {1} ".format(artifact_url,artifactory_age))
             delete_artifact(artifact_url)
