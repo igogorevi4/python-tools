064ea2aebb8f0436cad45dd5e64b36fe0a69311c
Yaroslav Pozdnyakov <yaroslav.pozdnyakov@gmail.com>
2020-02-15 08:47:35 +0000

    code optimization

--- simple-python-web-server-in-k8s/app.py
+++ simple-python-web-server-in-k8s/app.py
@@ -3,28 +3,23 @@
 # Application run as daemon with next rotes:
 # “/” with page where it is written  "Success!"
 # “/ping” where is written "Ok"
-# “/factorial”. Funciton, wich calculated factorial of number. The number posted with "POST" to this rote and calculate and return it.
+# “/factorial”. Function which calculates factorial of number. The number posted with "POST" to this rote and calculate and return it.
 
 
 from http.server import HTTPServer, BaseHTTPRequestHandler
 from io import BytesIO
 import socketserver
-
+import math
 
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
 
     def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
         if self.path == "/":
-            self.send_response(200)
-            self.end_headers()
             self.wfile.write(b"Success!")
         elif self.path == "/ping":
-            self.send_response(200)
-            self.end_headers()
             self.wfile.write(b"Ok")
-        else:
-            self.send_response(200)
-            self.end_headers()
 
     def do_POST(self):
         content_length = int(self.headers['Content-Length'])
@@ -35,15 +30,20 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
 
         try:
             number = int(body)
-            result = 1
-            if number >= 0:
-                i = 1 
-                for i in range (1, int(number)+1):
-                    result = result * i
-            else:
-                result = "Put the positive number or 0"
+            result = math.factorial(number)
         except:
-            result = "Put the positive number or 0"
+            result = "Non-negative number required"
+        # try:
+        #     number = int(body)
+        #     result = 1
+        #     if number >= 0:
+        #         i = 1 
+        #         for i in range (1, int(number)+1):
+        #             result = result * i
+        #     else:
+        #         result = "Non-negative number required"
+        # except:
+        #     result = "Non-negative number required"
             
         response.write(str(result).encode())
         self.wfile.write(response.getvalue())
