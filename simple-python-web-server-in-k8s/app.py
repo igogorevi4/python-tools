d039187b3d3d4cde74aa3aeae0f4db74832f82d3
Yaroslav Pozdnyakov <yaroslav.pozdnyakov@gmail.com>
2020-02-16 18:15:18 +0300

    fixed http headers

--- simple-python-web-server-in-k8s/app.py
+++ simple-python-web-server-in-k8s/app.py
@@ -15,17 +15,16 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
 
     def do_GET(self):
         if self.path == "/":
+            self.send_response(200)
+            self.end_headers()
             self.wfile.write(b"Success!")
-            self.send_response(200)
-            self.end_headers()
         elif self.path == "/ping":
-            self.wfile.write(b"Ok")
             self.send_response(200)
             self.end_headers()
+            self.wfile.write(b"Ok")
         else:
             self.send_response(404)
             self.end_headers()
-
 
     def do_POST(self):
         response = BytesIO()
