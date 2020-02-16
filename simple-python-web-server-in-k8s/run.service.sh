827bbeda06c602b8f5c534e812a1b0eb48edf707
Yaroslav <igogorevi4@users.noreply.github.com>
2020-02-16 18:40:12 +0300

    Update run.service.sh

--- simple-python-web-server-in-k8s/run.service.sh
+++ simple-python-web-server-in-k8s/run.service.sh
@@ -7,11 +7,11 @@ echo "========= Building an image of a simple python-web-server"
 $DOCKER build -t simple-python-app .
 
 cd k8s/
-echo "========= Creating simple web-cluster in kubernetes..."
+echo "========= Creating simple web-cluster in kubernetes"
 $KUBECTL apply -f mandatory.yaml && \
     $KUBECTL apply -f cloud-generic.yaml && \
     $KUBECTL apply -f app.yaml && \
     $KUBECTL apply -f ingress.yaml && \
-    echo "========= Waiting for cluster started ..."
+    echo "========= Waiting for cluster started"
     sleep 15 && \
     echo "========= Cluster has been started successfully."
